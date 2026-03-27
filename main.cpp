d07197be09c74b1343d8af7ee5ae85d83a35dde5
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-03-27 19:46:52 -0300

    salvação

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main(){
+
+    int a = 8;
+    //int numero inteiro
+    float b = 9.6;
+    //float numero de ponto flutuante ocupa 4bytes.
+    double c = 4.6;
+    //double ocupa 8bytes.
+
+
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
+
+    return 0;
+}
+
+  
+
+
