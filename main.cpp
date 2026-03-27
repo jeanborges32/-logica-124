edcf6451076ba8ad30df80c9de3405c841261d0f
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-03-27 20:07:48 -0300

    atualizado

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
-    int a = 8;
-    //int numero inteiro
-    float b = 9.6;
-    //float numero de ponto flutuante ocupa 4bytes.
-    double c = 4.6;
-    //double ocupa 8bytes.
-
-
-    std::cout<<a<<std::endl;
-    std::cout<<b<<std::endl;
-    std::cout<<c<<std::endl;
+    double c = 8.8654524545;
+    
+    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
 
     return 0;
 }
