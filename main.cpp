c358b9598ac7b168fa4feca1cc4b25e2080530d4
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-03-27 21:50:34 -0300

    renovação

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 int main(){
 
     std::string nome;
+    int idade;
+    float altura;
 
     std::cout<<"Qual seu nome: "<<std::endl;
     std::cin>>nome;
+    std::cout<<"Qual a sua idade: "<<std::endl;
+    std::cin>>idade;
+    std::cout<<"qual a sua altura: "<<std::endl;
+    std::cin>>altura;
 
-    std::cout<<"Bem-vindo(a) "<<nome<<"!"<<std::endl;
+    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+    std::cout<<"Sua idade "<<idade<<"anos. "<<std::endl;
+    std::cout<<"Sua altura "<<altura<<"!!"<<std::endl;
 
     
 
