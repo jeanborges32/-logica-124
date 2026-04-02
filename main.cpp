88a8cae57abf9373447fd16379bf0691340d6754
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-01 21:13:26 -0300

    raiva

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@ int main(){
     
     int result1 = 4 + 4 * 2;
     int result2 = 4 + 4 / 2;
+    int result3 = (4 +4) * 2;
 
     std::cout<<result1<<std::endl;
     std::cout<<result2<<std::endl;
+    std::cout<<result3<<std::endl;
 
 
 
