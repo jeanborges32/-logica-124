8b0574f41c452e07e00cd7a0d141d7fd4282f88a
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-01 21:54:40 -0300

    teste56

--- main.cpp
+++ main.cpp
@@ -3,14 +3,10 @@
 
 int main(){
 
-    int num1, num2, soma;
+      std::string nome = "Eduardo";
+      std::string sobrenome = "Barreto";
 
-    std::cout<<"Digite num 1: "<<"<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"\n\no num 1 é  "<<"\n"; 
-
-
+      std::cout<<nome + " " + sobrenome<<std::endl;
     
 
 
