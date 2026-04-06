de0e53c6806980c975835bc3c1435b861e878833
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-06 20:46:06 -0300

    segunda2

--- main.cpp
+++ main.cpp
@@ -3,13 +3,10 @@
 
 int main(){
 
-   int a = 40;
-   int b = 10;
-   std::string c = "40";
+   int a = 40; 
    std::string d = "10";
 
-   std::cout<<a+b<<std::endl;
-   std::cout<<c+d<<std::endl;
+  std::cout<<a+d<<std::endl;
 
 
       
