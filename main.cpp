9c4bc579e9808ab8623f12b294f254f9631967a6
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-08 21:54:42 -0300

    quarta9

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
-#include<cmath>
+#include<string>
 
 
 int main(){
 
-    int a = 5;
-    int b = 2;
+    std::string user;
+    std::string senha;
 
-    if(a > b){
-        std::cout<<"A é maior que B."<<std::endl;
-    }else{
-    std::cout<<"B é maior ou igual a A. "<<std::endl;    
+    std::cout<<"Digite seu usuario de acesso: "<<std::endl;
+    std::cin>>user;
 
-    }    
-    
+    std::cout<<"Digitee sua senha: "<<std::endl;
+    std::cin>>senha;
+
+    if(senha =="123456789"){
+        std::cout<<"O usuario "<<user<<" está logado com sucesso!!"<<std::endl;
+        }else{
+            std::cout<<"Usuario ou senha invalidos!"<<std::endl;
+        }    
 
    
       
