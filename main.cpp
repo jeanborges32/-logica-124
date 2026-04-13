d82ffb1fef3823e49e9dd30481c80fe21a3b38ee
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-13 19:47:19 -0300

    segunda35

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@ int main(){
         std::cout<<"Idade invalida!"<<std::endl;
     }else if(idade <= 2){
         std::cout<<"Voce é um bebe!"<<std::endl;
+    }else if(idade <= 8){
+        std::cout<<"Voce é uma criança!"<<std::endl;
+    }else if(idade <= 16){
+        std::cout<<"voce é um(a) adoleceste!"<<std::endl; 
+    }else if(idade <=60){
+        std::cout<<"Voce é adulto(a)!"<<std::endl;
+    }else{
+        std::cout<<"Voce é um(a) senior!"<<std::endl;
     }
     
     
