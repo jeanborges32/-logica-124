e85d4d948580010fd68e9adda6471e01d8231835
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-15 19:22:58 -0300

    quarta1

--- main.cpp
+++ main.cpp
@@ -1,51 +1,15 @@
 #include<iostream>
-#include<string>
-
-
 
 int main(){
 
 
-    std:: string nome;
-    float altura;
-    int idade;
-
-    std::cout<<"Qual é o seu nome ?"<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual sua altura ?"<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Qual a sua idade ?"<<std::endl;
-    std::cin>>idade;
-
-
-    
-
-   
-      
-
-    
-
-
-
-    
-
-    
-    
-
-    
-
-
-    
-
-    
-
-
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"Este é um loop!!"<<std::endl;
+    }
+     
+     
     return 0;
 }
-
-
 
  
 
