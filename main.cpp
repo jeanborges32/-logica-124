30408036e2955e1174986033d9348ff112e25674
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-15 20:37:37 -0300

    quarta5

--- main.cpp
+++ main.cpp
@@ -2,11 +2,38 @@
 
 int main(){
 
-    int tabuada = 7;
+    int opcao;
 
-    for(int i = 0; i <= 10; i++){
-        std::cout<<"O resultado de: "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+    std::cout<<"Digite um valor para opcao: "<<std::endl;
+    std::cin>>opcao;
+
+    switch(opcao){
+        case 1:
+            std::cout<<"Domingo!"<<std::endl;
+            break;
+        case 2:
+            std::cout<<"Segunda-feira!"<<std::endl;
+            break;
+        case 3:
+            std::cout<<"Terça-feira!"<<std::endl;
+            break;
+        case 4:
+            std::cout<<"Quarta_feira"<<std::endl;
+            break;
+        case 5:
+            std::cout<<"Quinta-feira"<<std::endl;
+            break;
+        case 6:
+            std::cout<<"Sexta-feira"<<std::endl;
+            break;
+        case 7:
+            std::cout<<"Sabado"<<std::endl;
+            break;
+          
+
     }
+
+    
 
 
 
