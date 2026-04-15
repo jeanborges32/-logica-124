eaf4ce89a852a6a2c2c376f2feaa7b5b730eb410
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-15 19:48:46 -0300

    quarta3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> // para definir unidades de tempo (ms, segundos)
 
 int main(){
 
 
     for(int i = 10; i >= 0; i--){
         std::cout<<"A bomba irá explodir em.."<<i<<std::endl;
+    
+
+       std::this_thread::sleep_for(std::chrono::milliseconds(500));
+       //seconda -> milliseconds
+
     }
      
      
