185aebe220a82d0bba429b68337c51dac92eba40
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-20 19:41:34 -0300

    feriado2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(){
     int idades[3] = {27, 31, 20};
 
     std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos!"<<std::endl;
+    std::cout<<nomes[2]<<" tem "<<idades[2]<<" anos!"<<std::endl;
 
 
     
