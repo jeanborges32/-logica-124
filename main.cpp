9859f1fd8bbb10a880937b4a4517c30c41478b85
JEAN CARLOS PINHEIRO BORGES <70508194253@senacrs.edu.br>
2026-04-20 20:11:51 -0300

    feriado6

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ int main(){
 
    std::string carros[5] = {"Volvo", "BMW", "Ford", "Chevrolet", "Fiat"};
 
-   for(int i = 0; i < 5; i++){
-       std::cout<<carros[i]<<std::endl;
-   }
+   int tamanho = sizeof(carros) / sizeof(carros[0]);
+
+   std::cout<<tamanho<<std::endl;
+
+   
     
 
     
